b586cc3ee0af384f3a7bb6a5422b52f177f22638
Seohyun Hwang <ih1520@inha.edu>
2025-10-12 19:29:09 +0900

    [Silver IV] Title: 큐 2, Time: 316 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue> 
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    
+	int n, a;
+	string s;
+	queue <int> q;
+	cin >> n;
+	for (int i = 0; i < n;i++) {
+
+		cin >> s;
+		if (s == "push") {
+			cin >> a;
+			q.push(a);
+		}
+		else if (s == "pop") {
+			if (q.empty()) {
+				cout << "-1" << '\n';
+			}
+			else {
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (s == "size") {
+			cout << q.size() << '\n';
+		}
+		else if (s == "empty") {
+			if (q.empty()) {
+				cout << "1" << '\n';
+			}
+			else cout << "0" << '\n';
+		}
+		else if (s == "front") {
+			if (q.empty()) {
+				cout << "-1" << '\n';
+			}
+			else cout << q.front() << '\n';
+		}
+		else if (s == "back") {
+
+			if (q.empty()) {
+				cout << "-1" << '\n';
+			}
+			else cout << q.back() << '\n';
+		}
+
+
+	}
+	
+
+
+}
