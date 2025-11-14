6a6972f455c3887d0d7c513b80297a1e98de27eb
Seohyun Hwang <ih1520@inha.edu>
2025-11-14 23:03:18 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <array>
+#include <stack>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	string id;
+	cin >> id;
+	cout << id << "??!";
+	
+	
+	return 0;
+}
